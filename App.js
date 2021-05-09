0093062c4a3fba65189b8acf1fc62da8ab0cc914
sean <seanpetermcelroy@gmail.com>
2021-05-09 21:53:37 +0100

    added the delete every item modal screen

--- App.js
+++ App.js
@@ -4,13 +4,24 @@ import { StyleSheet, Text, View, Platform, Modal} from 'react-native';
 import ShoppingList from './components/ShoppingList'
 import AddItemComp from './components/AddItemCom'
 import EmptyListCom from './components/EmptyListCom'
+import { Ionicons } from '@expo/vector-icons'
+import { FontAwesome5 } from '@expo/vector-icons';
+
 
 export default function App() {
   const [emptyModal, setOpenModal] = useState(false)
   return (
     <View style={styles.container}>
-      <Modal visible={emptyModal}>
-
+      <Modal 
+      animationType="slide"
+      visible={emptyModal}
+      transparent={true}
+      >
+        <View style={styles.modalstyl}>
+          <FontAwesome5 onPress={()=>setOpenModal(false)} name="backspace" size={60} color="white" style={styles.backbtn} />
+          <Text  style={styles.modalText}>This will permanantely delete everything</Text>
+          <Ionicons name="trash-bin" size={150} color="red" />
+        </View>
       </Modal>
       <Text style={styles.heading}>McElroy Shopping List</Text>
       <AddItemComp/>
@@ -24,7 +35,11 @@ export default function App() {
   );
 
   function openCloseModal(){
-    console.log("Opening modl")
+    setOpenModal(true)
+  }
+
+  function emptyList(){
+    console.log('emptying list')
   }
 }
 
@@ -51,5 +66,23 @@ const styles = StyleSheet.create({
   emptyListBTN:{
     marginTop: 20,
     width: '50%'
+  },
+  modalView:{
+    backgroundColor: 'blue'
+  },  
+  modalText:{
+    color: 'red'
+  },
+  modalstyl:{
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backbtn:{
+    position: 'absolute',
+    top: 0,
+    left: 10,
   }
 });
